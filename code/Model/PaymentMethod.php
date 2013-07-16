7ac838f2a16fef031fef6f14df9bf193974923a0
Chris Rasys <chris.rasys@blueacorn.com>
2013-07-16 14:21:31 -0400

    Adding dummy credit card functions from Greenspec.php

--- code/Model/PaymentMethod.php
+++ code/Model/PaymentMethod.php
@@ -104,4 +104,38 @@ class BlueAcorn_Greenspec_Model_PaymentMethod extends Mage_Payment_Model_Method_
 
         return $discount*-1;
     }
+
+    /**
+     * this method is called if we are just authorising
+     * a transaction
+     */
+    public function authorize (Varien_Object $payment, $amount)
+    {
+
+    }
+
+    /**
+     * this method is called if we are authorising AND
+     * capturing a transaction
+     */
+    public function capture (Varien_Object $payment, $amount)
+    {
+
+    }
+
+    /**
+     * called if refunding
+     */
+    public function refund (Varien_Object $payment, $amount)
+    {
+
+    }
+
+    /**
+     * called if voiding a payment
+     */
+    public function void (Varien_Object $payment)
+    {
+
+    }
 }
