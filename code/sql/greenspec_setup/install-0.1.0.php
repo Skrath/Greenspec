32c2357568e2caacf86f232c04a3fa739fe3bcfe
Chris Rasys <chris.rasys@blueacorn.com>
2013-07-16 14:23:58 -0400

    Removing Jim's installer file

--- code/sql/greenspec_setup/install-0.1.0.php
+++
@@ -1,5 +0,0 @@
-<?php
-$this->startSetup();
-$this->run("    ALTER TABLE  `".$this->getTable('sales/quote_address')."` ADD  `greenspec_amount` DECIMAL( 10, 2 ) NOT NULL;
-                ALTER TABLE  `".$this->getTable('sales/quote_address')."` ADD  `base_greenspec_amount` DECIMAL( 10, 2 ) NOT NULL;");
-$this->endSetup();
